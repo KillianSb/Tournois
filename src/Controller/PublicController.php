f8d2d6ce50f068bbf34b282c3b701da98b797b2d
Killian.Sb <TheKller29@Gmail.com>
2023-08-21 16:31:54 +0200

    init PublicController

---
+++ src/Controller/PublicController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PublicController extends AbstractController
+{
+    #[Route('/public', name: 'public_home')]
+    public function home(): Response
+    {
+        return $this->render('public/index.html.twig', [
+            'controller_name' => 'PublicController',
+        ]);
+    }
+}
