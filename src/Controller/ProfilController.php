2871fbce6b5ead09e36bcdc8c21509fa9ed501c6
Killian.Sb <TheKller29@Gmail.com>
2023-08-23 16:53:12 +0200

    Feat: Modification profil

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Repository\UserRepository;
+use Cassandra\Type\UserType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
---
+++ src/Form/UserType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email')
+            /*->add('roles')*/
+            /*->add('password')*/
+            ->add('username')
+            ->add('lastname')
+            ->add('firstname')
+            ->add('phoneNumber')
+            /*->add('isAdmin')*/
+            /*->add('isActive')*/
+            ->add('picture')
+            /*->add('teams')*/
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
