246fcdcba236b92f28bd0bb80c1b36771365d84f
Killian.Sb <TheKller29@Gmail.com>
2023-08-25 16:53:41 +0200

    Feat: desactivation d'un compte

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,11 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Bundle\SecurityBundle\Security;
 
 class SecurityController extends AbstractController
 {
     #[Route(path: '/connection', name: 'security_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(AuthenticationUtils $authenticationUtils, Security $security): Response
     {
          if ($this->getUser()) {
              return $this->redirectToRoute('public_home');
@@ -20,6 +21,14 @@ class SecurityController extends AbstractController
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
+
+        // Vérification de isActive
+        if ($error === null) {
+            $user = $security->getUser();
+            if ($user !== null && $user->isIsActive() === 0) {
+                $error = new \Symfony\Component\Security\Core\Exception\BadCredentialsException('Votre compte est désactivé !');
+            }
+        }
 
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
