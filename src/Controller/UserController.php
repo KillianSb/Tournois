3938234a6697c5e05d3ab7b3aa10eedc115fb6da
Killian.Sb <TheKller29@Gmail.com>
2023-08-28 16:12:35 +0200

    Feat : sécurité des routes avec vérification de l'identité

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -11,30 +11,42 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/utilisateur')]
 class UserController extends AbstractController
 {
 
     #[Route('/{id}/modifier', name: 'utilisateur_modifier', methods: ['GET', 'POST'])]
-    public function modifier(Request $request, User $user, EntityManagerInterface $entityManager): Response
+    #[IsGranted('ROLE_USER')]
+    public function modifier(Request $request, User $user, EntityManagerInterface $entityManager, Security $security): Response
     {
-        $form = $this->createForm(UserType::class, $user);
-        $form->handleRequest($request);
+        // Récupérer l'utilisateur connecté
+        $currentUser = $security->getUser();
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
+        // Vérifier si l'utilisateur est connecté et son ID correspond à l'ID passé en paramètre
+        if ($currentUser && $currentUser->getId() === $user->getId()) {
+            $form = $this->createForm(UserType::class, $user);
+            $form->handleRequest($request);
 
-            return $this->redirectToRoute('profil_infos', [], Response::HTTP_SEE_OTHER);
+            if ($form->isSubmitted() && $form->isValid()) {
+                $entityManager->flush();
+
+                return $this->redirectToRoute('profil_infos', [], Response::HTTP_SEE_OTHER);
+            }
+
+            return $this->render('user/modifier.html.twig', [
+                'user' => $user,
+                'form' => $form,
+            ]);
+        } else {
+            // L'utilisateur n'est pas autorisé à accéder à cette page
+            return $this->redirectToRoute('error_403', [], Response::HTTP_SEE_OTHER);
         }
-
-        return $this->render('user/modifier.html.twig', [
-            'user' => $user,
-            'form' => $form,
-        ]);
     }
 
-    #[Route('supprimer/{id}', name: 'utilisateur_supprimer', methods: ['POST'])]
+    #[Route('/supprimer/{id}', name: 'utilisateur_supprimer', methods: ['POST'])]
+    #[IsGranted('ROLE_ADMIN')]
     public function supprimer(Request $request, User $user, EntityManagerInterface $entityManager): Response
     {
         if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->request->get('_token'))) {
@@ -45,14 +57,24 @@ class UserController extends AbstractController
         return $this->redirectToRoute('public_home', [], Response::HTTP_SEE_OTHER);
     }
 
-    #[Route('desactiver/{id}', name: 'utilisateur_desactiver', methods: ['GET','POST'])]
+    #[Route('/desactiver/{id}', name: 'utilisateur_desactiver', methods: ['GET','POST'])]
+    #[IsGranted('ROLE_USER')]
     public function desactiverUtilisateur(Request $request, User $user, EntityManagerInterface $entityManager, Security $security): Response
     {
-        // Désactiver l'utilisateur actuel
-        $security->getUser()->setIsActive(0);
-        $entityManager->flush();
+        // Récupérer l utilisateur connecté
+        $currentUser = $security->getUser();
 
-        // Déconnexion de l'utilisateur actuel
-        return $this->redirectToRoute('security_logout', [], Response::HTTP_SEE_OTHER);
+        // Vérifier si l'utilisateur est connecté et son ID correspond à l'ID passé en paramètre
+        if ($currentUser && $currentUser->getId() === $user->getId()) {
+            // Désactiver l'utilisateur actuel
+            $currentUser->setIsActive(false);
+            $entityManager->flush();
+
+            // Déconnexion de l'utilisateur actuel
+            return $this->redirectToRoute('security_logout', [], Response::HTTP_SEE_OTHER);
+        } else {
+            // L'utilisateur n'est pas autorisé à accéder à cette page
+            return $this->redirectToRoute('error_403', [], Response::HTTP_SEE_OTHER);
+        }
     }
 }
