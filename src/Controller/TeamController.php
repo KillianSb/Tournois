fa67e7f735fc76372233c6071e3b5767db72c0e4
Killian.Sb <TheKller29@Gmail.com>
2023-08-29 11:30:04 +0200

    réso erreur

--- src/Controller/TeamController.php
+++ src/Controller/TeamController.php
@@ -11,6 +11,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 
 #[Route('/equipe')]
